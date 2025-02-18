62e92b6f860c0164c12659d0e82352403b13e28a
Setu <aggarwalsetu.1@gmail.com>
2025-02-18 15:48:50 +0530

    new updated on 4

--- test_cases/conftest.py
+++ test_cases/conftest.py
@@ -13,23 +13,22 @@ def setup():
 
 
 
-import pytest
+# import pytest
 
 
-@pytest.hookimpl(hookwrapper=True)
-def pytest_runtest_makereport(item, call):
+# @pytest.hookimpl(hookwrapper=True)
+# def pytest_runtest_makereport(item, call):
     """
     Hooks into each test and attaches highlighted outputs to HTML reports.
     """
-    outcome = yield
-    report = outcome.get_result()
+    # outcome = yield
+    # report = outcome.get_result()
 
-    if report.when == "call" and report.passed:  # Highlight for passed tests
+    # if report.when == "call" and report.passed:  # Highlight for passed tests
         # Simulate a verified output to highlight in the report
-        verified_output = "<b style='color: green;'>Verified Output: Test Passed!</b>"
-        report.extra = getattr(report, "extra", [])
-        report.extra.append(pytest_html.extras.html(verified_output))
-
+        # verified_output = "<b style='color: green;'>Verified Output: Test Passed!</b>"
+        # report.extra = getattr(report, "extra", [])
+        # report.extra.append(pytest_html.extras.html(verified_output))
 
 
 
